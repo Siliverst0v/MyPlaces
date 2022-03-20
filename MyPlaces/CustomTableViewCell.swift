be04085ca68bcd6f634146982e376f541e6d24b2
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-03-21 00:09:39 +0300

    7 lesson Custom TableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Анатолий Силиверстов on 20.03.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+}
