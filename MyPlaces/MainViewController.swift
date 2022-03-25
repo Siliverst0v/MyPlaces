f17c198d59a733e4d291bd7b1add698d8af4db76
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-03-25 23:27:42 +0300

    19 lesson edit and save

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -6,71 +6,80 @@
 //
 
 import UIKit
-
+import RealmSwift
 
 class MainViewController: UITableViewController {
     
     
-//    var places = Place.getPlaces()
+    var places: Results<Place>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        places = realm.objects(Place.self)
     }
 
     // MARK: - Table view data source
 
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//        return places.count
-//    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return places.isEmpty ? 0 : places.count
+    }
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-//
-//        let place = places[indexPath.row]
-//
-//        cell.nameLabel?.text = place.name
-//        cell.locationLabel.text = place.location
-//        cell.typeLabel.text = place.type
-//
-//        if place.image == nil {
-//            cell.imageOfPlace?.image = UIImage(named: place.restaurantImage!)
-//        } else {
-//            cell.imageOfPlace?.image = place.image
-//        }
-//
-//
-//        cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
-//        cell.imageOfPlace?.clipsToBounds = true
-//
-//        return cell
-//    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+
+        let place = places[indexPath.row]
+
+        cell.nameLabel?.text = place.name
+        cell.locationLabel.text = place.location
+        cell.typeLabel.text = place.type
+        cell.imageOfPlace.image = UIImage(data: place.imageData!)
+
+
+        cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace?.clipsToBounds = true
+
+        return cell
+    }
     
     // MARK: - Table view delegate
     
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 85
-//    }
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+       
+       let place = places[indexPath.row]
+       let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") {  (contextualAction, view, boolValue) in
+
+           StorageManager.deleteObject(place)
+           tableView.deleteRows(at: [indexPath], with: .automatic)
+       }
+        
+        
+        
+       let swipeActions = UISwipeActionsConfiguration(actions: [deleteAction])
+
+       return swipeActions
+   }
 
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let place = places[indexPath.row]
+            let newPlaceVC = segue.destination as! NewPlaceViewController
+            newPlaceVC.currentPlace = place
+        }
+        
     }
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
         guard let newPlaceVC = segue.source as? NewPlaceViewController else {return}
         
-        newPlaceVC.saveNewPlace()
-        
-//        places.append(newPlaceVC.newPlace!)
+        newPlaceVC.savePlace()
         tableView.reloadData()
     }
 
--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController, UINavigationControllerDelegate {
     
-    var newPlace = Place()
+    var currentPlace: Place?
+    
     var imageIsChanged = false
     
     @IBOutlet weak var placeImage: UIImageView!
@@ -21,13 +22,10 @@ class NewPlaceViewController: UITableViewController, UINavigationControllerDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
         saveButton.isEnabled = false
-        
-        DispatchQueue.main.async {
-            self.newPlace.savePlaces()
-        }
        
-        
         placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
+        setupEditScreen()
         
     }
     
@@ -66,7 +64,7 @@ class NewPlaceViewController: UITableViewController, UINavigationControllerDeleg
         }
         
     }
-    func saveNewPlace() {
+    func savePlace() {
         
         var image: UIImage?
         
@@ -76,12 +74,53 @@ class NewPlaceViewController: UITableViewController, UINavigationControllerDeleg
             image = #imageLiteral(resourceName: "imagePlaceholder")
         }
         
-//        newPlace = Place(name: placeName.text!,
-//                         location: placeLocation.text,
-//                         type: placeType.text,
-//                         image: image,
-//                         restaurantImage: nil)
+        let imageData = image?.pngData()
+        
+        let newPlace = Place(name: placeName.text!,
+                             location: placeLocation.text,
+                             type: placeType.text,
+                             imageData: imageData)
+        
+        if currentPlace != nil {
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
+        StorageManager.saveObject(newPlace)
+        }
     }
+    
+    private func setupEditScreen() {
+        
+        if currentPlace != nil {
+            
+            setupNavigationBar()
+            imageIsChanged = true
+            
+            guard let data = currentPlace?.imageData, let image = UIImage(data: data) else {return}
+            
+            placeImage.image = image
+            placeImage.contentMode = .scaleAspectFill
+            
+            placeName.text = currentPlace?.name
+            placeLocation.text = currentPlace?.location
+            placeType.text = currentPlace?.type
+            
+        }
+    }
+    
+    private func setupNavigationBar() {
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+    }
+    
     
     @IBAction func cancelAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
